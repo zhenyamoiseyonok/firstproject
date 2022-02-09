0df5dc7fc6d0d40af88c1dc8295b012ffcb30ba2
zhenyamoiseyonok <zhekamoiseyonok98@gmail.com>
2022-02-09 21:41:30 +0300

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//int a=6;
+//if (a>5) {
+        //   System.out.println("Число больше 5");
+//} else {
+        //   System.out.println("ne bolshe 5");
+//
+//}
+        //      System.out.println("Программа закончена");
+        // }
+//        int a = 0;
+//        if (a < 0) {
+//            System.out.println("chislo otr");
+//        } else if (a == 0) {
+//            System.out.println("ravno0");
+//        } else {
+//            System.out.println("polozh");
+//        }
+
+    }
+}
